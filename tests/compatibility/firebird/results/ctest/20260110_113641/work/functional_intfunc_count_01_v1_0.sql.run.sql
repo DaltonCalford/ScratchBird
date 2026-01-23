ff2a414e5a7c4400a27d258a5e1847ba51aefa57
Dalton Calford <dalton.calford@gmail.com>
2026-01-23 13:37:04 -0500

    Remove test artifacts and ignore ctest results

--- tests/compatibility/firebird/results/ctest/20260110_113641/work/functional_intfunc_count_01_v1_0.sql.run.sql
+++
@@ -1,8 +0,0 @@
--- === PRELUDE SCRIPT ===
-CREATE DATABASE '/home/dcalford/CliWork/ScratchBird/tests/compatibility/firebird/results/ctest/20260110_113641/work/functional_intfunc_count_01_v1_0.sql.fdb';
-CONNECT '/home/dcalford/CliWork/ScratchBird/tests/compatibility/firebird/results/ctest/20260110_113641/work/functional_intfunc_count_01_v1_0.sql.fdb';
--- === INIT SCRIPT ===
-CREATE TABLE test( id INTEGER);
-
--- === TEST SCRIPT ===
-SELECT COUNT(*) FROM test;
--- tests/compatibility/firebird/results/ctest/20260122_142917/work/functional_intfunc_avg_08.sql.run.sql
+++
@@ -1,9 +0,0 @@
--- === PRELUDE SCRIPT ===
-CREATE DATABASE '/home/dcalford/CliWork/ScratchBird/tests/compatibility/firebird/results/ctest/20260122_142917/work/functional_intfunc_avg_08.sql.fdb';
-CONNECT '/home/dcalford/CliWork/ScratchBird/tests/compatibility/firebird/results/ctest/20260122_142917/work/functional_intfunc_avg_08.sql.fdb';
--- === INIT SCRIPT ===
-CREATE TABLE test( id INTEGER);
-INSERT INTO test VALUES(NULL);
-
--- === TEST SCRIPT ===
-SELECT AVG(id) FROM test;
